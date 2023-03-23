aec26838b0d57286e8a7a9d84b70203f674639c0
HenriqueSam45Br <127551165+HenriqueSam45Br@users.noreply.github.com>
2023-03-23 08:38:43 -0300

    Add files via upload

---
+++ colections 1 e 2/colection_1.java
@@ -0,0 +1,29 @@
+package colection_1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class colection_1 {
+
+	public static void main(String[] args) {
+
+		ArrayList cores = new ArrayList();
+		Scanner leia = new Scanner(System.in);
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("\n Digite a cor que você deseja adicionar à lista: ");
+			String cor = leia.nextLine();
+
+			cores.add(cor);
+
+		}
+
+		System.out.println("\n Todas as cores adicionadas: " + cores);
+
+		Collections.sort(cores);
+		System.out.println("\n Cores da lista em ordem alfabética: " + cores);
+
+	}
+
+}
